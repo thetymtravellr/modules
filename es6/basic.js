eced31e2223bba309775cb32f578b1018afd2c8b
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-02-18 22:56:40 +0600

    module 31

---
+++ es6/basic.js
@@ -0,0 +1,14 @@
+// variable
+var deposit = 400;
+// condition
+if(deposit > 500){}
+if(deposit < 200){}
+else if(deposit == 500){}
+else if(deposit != 500){}
+if(deposit >= 500){}
+if(deposit <= 500){}
+else {}
+// array
+const arr = [];
+let numbers = [5,7,1,8,2,5,6,5];
+var sum = [] // we should never use var
